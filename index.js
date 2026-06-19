06ef4e4e6e5677f7a2a8b624aecc83540a2e9740
devhanzz <devhanzero@gmail.com>
2026-06-19 22:19:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,36 @@ let recentVisitors = [];
 io.on('connection', (socket) => {
     const ua = socket.handshake.headers['user-agent'] || '';
     let device = "Desktop PC";
-    if (/android/i.test(ua)) device = "Android Phone";
-    else if (/iPhone|iPad|iPod/i.test(ua)) device = "iPhone/iOS";
+
+    if (/android/i.test(ua)) {
+        device = "Android Phone";
+        if (/samsung|sm-|gt-|sch-/i.test(ua)) device = "Samsung Phone";
+        else if (/redmi|xiaomi|poco|m20|m21|2201|2312/i.test(ua)) device = "Xiaomi/Redmi/Poco";
+        else if (/oppo|cph|pcfm|phtm|pbgm|pact/i.test(ua)) device = "Oppo Phone";
+        else if (/vivo|v20|v21|v22|v23|v19|v18|iqq/i.test(ua)) device = "Vivo Phone";
+        else if (/realme|rmx[0-9]/i.test(ua)) device = "Realme Phone";
+        else if (/huawei|eml-|jny-|vog-|pot-|ala-/i.test(ua)) device = "Huawei Phone";
+        else if (/infinix|x6[0-9]|x67/i.test(ua)) device = "Infinix Phone";
+        else if (/tecno|kg5|ki5|kf6|kh6|le7/i.test(ua)) device = "Tecno Phone";
+        else if (/oneplus|one plus|nord|be2|iv2/i.test(ua)) device = "OnePlus Phone";
+        else if (/google|pixel/i.test(ua)) device = "Google Pixel";
+        else if (/sony|xperia|so-[0-9]/i.test(ua)) device = "Sony Xperia";
+        else if (/asus|zenfone|rog phone/i.test(ua)) device = "Asus Phone/ROG";
+        else if (/lenovo|legion/i.test(ua)) device = "Lenovo Phone";
+        else if (/motorola|moto /i.test(ua)) device = "Motorola Phone";
+        else if (/htc|desire/i.test(ua)) device = "HTC Phone";
+        else if (/nokia/i.test(ua)) device = "Nokia Android";
+        else if (/blackshark|shark /i.test(ua)) device = "Black Shark Phone";
+        else if (/itel/i.test(ua)) device = "Itel Phone";
+    } 
+    else if (/iPhone/i.test(ua)) {
+        device = "iPhone";
+        if (/iPhone15|iPhone16|iPhone17/i.test(ua)) device = "iPhone (New Model)";
+    }
+    else if (/iPad/i.test(ua)) device = "iPad";
     else if (/Windows/i.test(ua)) device = "Windows PC";
     else if (/Macintosh/i.test(ua)) device = "MacBook";
+    else if (/linux/i.test(ua)) device = "Linux PC";
 
     let country = socket.handshake.headers['x-viewer-country'] || "Philippines"; 
 
